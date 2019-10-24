991f4ed34eecd0391e619bcab252f510b50b6d99
Hector Diaz <diazhector98@gmai.com>
2019-10-24 14:15:06 -0500

    Creating build file

---
+++ build.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+from keras.models import model_from_json
+from keras.models import load_model
+import matplotlib.pyplot as plt
